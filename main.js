e36db116485747902b9c33062949b7a0943ec7a7
vova <exemple@mail.ru>
2022-10-03 17:26:19 +0200

    Vipolneno pervoe zadanie

--- main.js
+++ main.js
@@ -11,3 +11,23 @@ answers[1] = prompt('Какая у вас фамилия', '')
 answers[2] = prompt('Сколько вам лет', '')
 
 document.write(answers)
+
+const  numberFilms = prompt('Сколько фильмов вы уже посмотрели',' ');
+
+const pesonalMovieBD = {
+    count: numberFilms,
+    movies: {},
+    actoros: {},
+    genres: [],
+    privat: false
+
+};
+const a = prompt('Один из последних просмотренных фильмов', ' '),
+      b = prompt('На сколько его оцените', ' '),
+      c = prompt('Один из последних просмотренных фильмов', ' '),
+      d = prompt('На сколько его оцените', ' '); 
+
+      pesonalMovieBD.movies[a] = b;
+      pesonalMovieBD.movies[c] = d
+
+      console.log (pesonalMovieBD)
