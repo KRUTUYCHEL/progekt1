6e66fe49cfe9182bc654437b8c49fed1c40c0840
vova <exemple@mail.ru>
2022-10-03 13:45:09 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//alert ('Hello')
+//const result = confirm("Are you here")
+//console.log (result)
+//const answer = prompt ("you are 18","18")
+//console.log (typeof(answer))
+
+const answers = []
+
+answers[0] = prompt('Как ваше имя', '')
+answers[1] = prompt('Какая у вас фамилия', '')
+answers[2] = prompt('Сколько вам лет', '')
+
+document.write(answers)
